16f7625f737647338021eb4f07cbd73336708e69
Aman Shrivastava <aman.srivastava999@gmail.com>
2014-12-13 21:28:47 +0530

    added main module

---
+++ js/main.js
@@ -0,0 +1,14 @@
+(function(){
+    var app = angular.module('subjects', []);
+    
+    app.controller('display', function(){
+        this.subject = people;
+    });
+    
+    var people = {
+        description: '',
+        story: '',
+        image:'',
+        size:''
+    }
+})();
